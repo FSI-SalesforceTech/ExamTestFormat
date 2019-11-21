8ad58a3decbae7b185cfb070c34d1ef1ad82c2bd
小倉 裕之 <ogurah@fsi.co.jp>
2019-11-21 18:00:50 +0900

    選択した行のチェックはついたままではあるものの、基本的な機能は完成

---
+++ force-app/main/default/lwc/examTestFormat/examTestFormat.js
@@ -0,0 +1,113 @@
+import { LightningElement, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getExistExam from '@salesforce/apex/ExamTestFormatController.getExistExam';
+import geteqQuestions from '@salesforce/apex/ExamTestFormatController.geteqQuestions';
+import geteqAnswers from '@salesforce/apex/ExamTestFormatController.geteqAnswers';
+import { refreshApex } from '@salesforce/apex';
+
+const columns = [
+    {label : '解答', fieldName : 'Answer__c'},
+];
+
+export default class examTestFormat extends LightningElement {
+    @track existExam;
+    @track selectedExam;
+    @track examdatas = [];
+    @track answers;
+    @track columns = columns;
+    @track isUnderTest = false;
+    examtemp = [];
+    @track dispExam;
+    examnumber = 0;
+    selectedanswer;
+    @track dispnum = 1;
+    @track totalnum;
+    @track progressnum = 0;
+    @track selectedRows = [];
+    @track isMark = false;
+    @track isResult = false;
+    @track passedNum = 0;
+    @track passedPer = 0;
+    @track isDrawing = false;
+
+    connectedCallback() {
+        getExistExam()
+            .then(result => {
+                this.existExam = [];
+                result.forEach(element => {
+                    this.existExam.push({label: element.ExamType__c, value: element.ExamType__c});
+                });
+                if(this.existExam.length === 1) this.isMark = true;
+            });
+    }
+
+    handleChange(e) {
+        this.selectedExam = e.detail.value;
+    }
+
+    async startExamFormat() {
+        this.examtemp = await geteqQuestions({'examname': this.selectedExam});
+        this.examdatas = [];
+
+        this.examtemp.forEach(element => {
+                geteqAnswers({'Id': element.Id})
+                    .then(result => {
+                        let answer = result.filter(ans => {return ans.isAnswer__c === true});
+                        this.examdatas.push({exam: element, selection: result, answers: answer});
+                        this.totalnum = this.examdatas.length;
+                        this.progressnum = Math.floor(100 / this.totalnum);
+                        this.dispExam = this.examdatas[this.examnumber];
+                    });
+            });
+        
+        this.isUnderTest = true;
+    }
+
+    handleNext() {
+        this.isDrawing = true;
+                
+        this.examdatas[this.examnumber]['selected'] = this.selectedanswer;
+        this.examnumber ++;
+        this.dispnum = this.examnumber + 1;
+        if(this.dispnum === this.totalnum) {
+            this.isMark = true;
+        }
+        this.progressnum = Math.floor((100 / this.totalnum) * this.dispnum);
+        this.dispExam = this.examdatas[this.examnumber];
+        this.isDrawing = false;
+    }
+
+    handlePrevious() {
+        if(this.examnumber !== 0) {
+            this.examnumber --;
+            this.dispExam = this.examdatas[this.examnumber];
+        }
+    }
+    
+    handleRowAction(event) {
+        this.selectedanswer = event.detail.selectedRows;
+    }
+    
+    handleMark() {
+        this.examdatas[this.examnumber]['selected'] = this.selectedanswer;        
+        this.examdatas.forEach(element => {
+            let isPassed = false;
+            if(element.answers.length === element.selected.length) {
+                isPassed = element.selected.every(item => {
+                    return item.isAnswer__c === true;
+                });
+            }
+            element['isPassed'] = isPassed;
+            if(isPassed === true){
+                this.passedNum ++;
+            }
+        });
+        this.passedPer = Math.floor(this.passedNum / this.totalnum * 100);
+        this.isResult = true;
+    }
+
+    gotoTop() {
+        this.isResult = false;
+        this.isUnderTest = false;
+    }
+}
